a9987e6a965125e615ddab3d6b943268edfbc080
Abhishek Gujjar <abhigujjar123@gmail.com>
2024-04-12 11:26:52 +0530

    updates

--- test.py
+++
@@ -1,6 +0,0 @@
-from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
-
-cust_data_obj=CustomData(1.7,62.6,59.0,7.65,7.61,4.77,"Premium","G","VS2")
-
-trial=cust_data_obj.get_data_as_dataframe()
-print(trial)
